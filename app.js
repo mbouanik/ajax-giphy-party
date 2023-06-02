63fc78e00f623f34fff2e2a14ffd45320f5db61a
mbouanik <mbouanik@student.42.fr>
2023-06-02 15:22:52 -0500

    backend done

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log("Let's get this party started!");
+const section = $('#giphys')
+const btn = $('#remove').on('click', ()=> section.children().remove())
+const input = $('#giphy-name')
+
+const form = $('#form')
+
+form.on('submit', (evt) => {
+    evt.preventDefault()
+    getGiphy()
+})
+
+async function getGiphy(){
+    const res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${input.val()}`, {params:{api_key:"MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"}})
+    console.log(res.data.data)
+    const {url} = res.data.data[parseInt(Math.random() * 12)].images.downsized_large
+    $(`<img src=${url}/>`).appendTo(section)
+    input.val("")
+
+}
